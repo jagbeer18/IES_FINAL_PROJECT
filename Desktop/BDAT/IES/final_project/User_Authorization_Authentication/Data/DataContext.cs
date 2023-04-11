5077b103b0df6aa86282a4da1076247ec0e2832f
jagbeer <jagbeer18@gmail.com>
2023-04-11 00:16:44 -0400

    message

---
+++ Desktop/BDAT/IES/final_project/User_Authorization_Authentication/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿
+using User_Authorization_Authentication.Models;
+
+
+namespace User_Authorization_Authentication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder
+                .UseSqlServer("Server=.\\jagbe;Database=userdb;Trusted_Connection=true;");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
